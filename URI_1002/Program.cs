f8886fe5c8c7e3c77688c3a871ab85556744dfb5
Rodrigo Pereira <rodrigo55pereira@gmail.com>
2020-04-22 23:11:40 -0300

    Exe uri_1002, uri_1003, uri_1007

---
+++ URI_1002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = 3.14159 * Math.Pow(raio, 2);
+
+            Console.WriteLine("A={0} ", area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
