f6b46dc6db874feb30207b0880af2cf2cb59807c
Rodrigo Pereira de Moraes <rodrigo@MacBook-Pro.local>
2020-05-28 10:59:45 -0300

    criacao projeto teste

--- MyTesteApp/obj/Debug/netcoreapp3.1/MyTesteApp.AssemblyInfo.cs
+++ MyTesteApp/obj/Debug/netcoreapp3.1/MyTesteApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("URI_1007")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MyTesteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("URI_1007")]
-[assembly: System.Reflection.AssemblyTitleAttribute("URI_1007")]
+[assembly: System.Reflection.AssemblyProductAttribute("MyTesteApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MyTesteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
