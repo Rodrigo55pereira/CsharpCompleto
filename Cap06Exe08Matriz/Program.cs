9e80b4f86be12f33d504c5fe95eaefa599ee6c3d
Rodrigo Pereira de Moraes <rodrigo@MacBook-Pro.local>
2020-06-05 16:43:28 -0300

    Atualizacao

---
+++ Cap06Exe08Matriz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cap06Exe08Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2,3];
+
+            Console.WriteLine(mat.Length);
+
+            // Rank - retorna a quantidade de linhas da matriz.
+
+            Console.WriteLine(mat.Rank);
+
+            // GetLength(0) - retorna a quantidade de linhas da matriz 
+
+            Console.WriteLine(mat.GetLength(0));
+
+            // GetLength(1) - Retorno a quantidade de colunas.
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
---
+++ Cap06ExeProp09Matriz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cap06ExeProp09Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            // Matriz
+            int[,] mat = new int[n,n];
+
+            for(int i = 0; i < n; i++)
+            {
+                
+            }
+
+        }
+    }
+}
