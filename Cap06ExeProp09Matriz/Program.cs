187497954517003fa0ab716ec6efdadc5a0beb74
Rodrigo Pereira de Moraes <rodrigo@MacBook-Pro.local>
2020-06-08 15:07:20 -0300

    Atualizacao execicio proposto de matriz

--- Cap06ExeProp09Matriz/Program.cs
+++ Cap06ExeProp09Matriz/Program.cs
@@ -9,12 +9,36 @@ namespace Cap06ExeProp09Matriz
             int n = int.Parse(Console.ReadLine());
 
             // Matriz
-            int[,] mat = new int[n,n];
+            int[,] mat = new int[n, n];
 
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
             }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int count  = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {   
+                    if(mat[i, j] < 0)
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Negative numbers: " + count);
 
         }
     }
