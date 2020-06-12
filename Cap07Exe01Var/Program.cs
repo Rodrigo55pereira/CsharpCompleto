a919af7546a0e50de2a7f7658fbcc35fbbc80cc1
Rodrigo Pereira de Moraes <rodrigo@MacBook-Pro.local>
2020-06-12 12:50:59 -0300

    Atualizacao ate funcao de string

---
+++ Cap07Exe01Var/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cap07Exe01Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            var w = z;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
